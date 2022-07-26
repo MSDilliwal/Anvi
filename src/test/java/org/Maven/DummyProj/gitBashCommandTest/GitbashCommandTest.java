c22613c909c0c1b5205057574c60a17524e742b1
MSDilliwal <marutisdilliwal97@gmail.com>
2022-07-26 11:56:45 +0530

    First Commit

---
+++ src/test/java/org/Maven/DummyProj/gitBashCommandTest/GitbashCommandTest.java
@@ -0,0 +1,9 @@
+package org.Maven.DummyProj.gitBashCommandTest;
+
+public class GitbashCommandTest {
+	public void gitBashCommand() {
+		System.out.println("Main Begin !");
+		
+		System.out.println("Main End !");
+	}
+}
